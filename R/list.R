f7ad8f2c3827652093c4ca92361707ecd9da97d4
R. van Harrevelt <R. van Harrevelt,042,133,,Rob,599>
2016-01-26 13:46:07 +0100

    Package zoo is now imported in zoo.R.

--- R/list.R
+++ R/list.R
@@ -1,6 +1,7 @@
 #' Convert a regts to a list of univariate regts objects
 #' @param x a \code{regts} object
 #' @return a list of univariate \code{regts} objects
+#' @export
 as.list.regts <- function(x, ...) {
     x <- NextMethod("as.list")
     lapply(x, FUN = as.regts)
