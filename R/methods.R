a3437f684b5861a2c337e043827c0a3ad70aec56
robvanharrevelt <rvanharrevelt@gmail.com>
2016-06-07 00:10:42 +0200

    regts.intersect, regts.union and cbind.regts now conserve the labels.

--- R/methods.R
+++ R/methods.R
@@ -17,23 +17,57 @@ window.regts <- function(x, ...) {
     return (as.regts(NextMethod(.Generic)))
 }
 
-#' @export
-regts.union <- function(...) {
-    return (as.regts(ts.union(...)))
+# Returns the timeseries label from an arbitrary object.
+# Only regts objects have labels
+get_labels <- function(x) {
+    if (is.regts(x)) {
+        return (ts_labels(x))
+    } else {
+        nc <- ncol(x)
+        if (!is.null(nc)) {
+            return (rep("", nc))
+        } else {
+            return ("")
+        }
+    }
 }
 
-#' @export
-regts.intersect <- function(...) {
-    return (as.regts(ts.intersect(...)))
+# Check if the arguments in ... contain any label, and if so than add
+# all labels to x
+handle_labels <- function(x, ...) {
+    arguments <- list(...)
+    # Check if there are any labels
+    has_labels <- any(unlist(lapply(arguments,
+                                    FUN = function(x) !is.null(ts_labels(x)))))
+    if (has_labels) {
+        labels <- unname(unlist(lapply(arguments, FUN = get_labels)))
+        ts_labels(x) <- labels
+    }
+    return(x)
 }
 
-
-#' cbind
+#' regts.union
 #'
 #' @export
- cbind.regts <- function(...) {
-    # we cannot simply use .Generic, because the method dispatching is not done via UserMethod(),
-    # but by C-internal patching. Therefore use the following tric:
+regts.union <- function(...) {
+    ret <- as.regts(ts.union(...))
+    ret <- handle_labels(ret, ...)
+    return (ret)
+}
+
+#' regts.intersect
+#' @export
+regts.intersect <- function(...) {
+    ret <- as.regts(ts.intersect(...))
+    ret <- handle_labels(ret, ...)
+    return (ret)
+}
+
+#' @export
+cbind.regts <- function(...) {
+    # we cannot simply use .Generic, because the method dispatching is not
+    # done via UserMethod(), but by C-internal patching. Therefore use the
+    # following tric:
     arguments <- list(...)
     names(arguments) <- eval(substitute(alist(...)))
 
@@ -45,9 +79,9 @@ regts.intersect <- function(...) {
             return (x)
        }
      }
-     arguments <- lapply(timeseries, FUN = f)
-
-     return (as.regts(do.call(cbind, timeseries)))
+     arguments <- lapply(arguments, FUN = f)
+     ret <- as.regts(do.call(cbind, arguments))
+     ret <- handle_labels(ret, ...)
 }
 
 #' @export
