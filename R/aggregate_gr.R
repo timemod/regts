510c7766cf7c69a7cf60724ac3475324104affcf
robvanharrevelt <rvanharrevelt@gmail.com>
2016-06-19 16:07:53 +0200

    Improved the error handling and the documentation.

--- R/aggregate_gr.R
+++ R/aggregate_gr.R
@@ -1,6 +1,18 @@
+#' Converts timeseries with absolute or relative growth rates to a lower
+#' frequency
+#'
+#' @param x  a \link{ts} of \link{regts} object
+#' @param frequency the frequency of the result, should be higher than
+#' the frequency of timeseries \code{x}
+#' @return a \link{regts} with lower frequency
+#' @export
 #' @useDynLib regts
 #' @importFrom Rcpp sourceCpp
-#' @export
 aggregate_gr <- function(x, frequency) {
+    if (!inherits(x, 'ts')) {
+        stop("Argument x is not a timeseries")
+    }
+
+    # call C++ function agg_gr (see src/agg_gr.cpp)
     return (agg_gr(x, frequency))
 }
--- src/agg_gr.cpp
+++ src/agg_gr.cpp
@@ -9,7 +9,6 @@ NumericMatrix create_ts(NumericMatrix x, int first, int last, int freq,
 // [[Rcpp::export]]
 NumericMatrix agg_gr(NumericMatrix ts_old, int freq_new) {
 
-
     // save the dimension names
     List dimnames = ts_old.attr("dimnames");
     CharacterVector names = dimnames[1];
@@ -22,21 +21,30 @@ NumericMatrix agg_gr(NumericMatrix ts_old, int freq_new) {
     int last_new = (last_old - (rep - 1)) / rep;
     int nper_new = last_new - first_new + 1;
 
+    if (freq_old % freq_new != 0) {
+        Rf_error("The new frequency %d is not a divisor of the old frequency "
+                 "%d\n", freq_new, freq_old);
+    }
+    if (nper_new <= 0) {
+        Rf_error("Cannot perform aggregation because the input timeseries "
+                "contains too few observations");
+    }
+
     NumericMatrix data(nper_new, ts_old.ncol());
 
     int shift = first_new * rep - (rep - 1) - first_old;
-    
-    for (int col = 0; col < ts_old.ncol(); col++) {    
+
+    for (int col = 0; col < ts_old.ncol(); col++) {
         for (int row = 0; row < nper_new; row++) {
             for (int i = 0; i < rep; i++) {
                 for (int j = 0; j < rep; j++) {
-                    data(row, col) = data(row, col) + 
+                    data(row, col) = data(row, col) +
                          ts_old(i + j + rep * row + shift, col);
                 }
             }
         }
     }
-    
+
     data = data / rep; // cgr method
 
     NumericMatrix ts_new = create_ts(data, first_new, last_new, freq_new,
@@ -75,9 +83,9 @@ int get_frequency(NumericMatrix ts) {
     return freq;
 }
 
-NumericMatrix create_ts(NumericMatrix x, int first, int last, int freq, 
+NumericMatrix create_ts(NumericMatrix x, int first, int last, int freq,
                         CharacterVector names) {
-        
+
     // get start and end vectors
     Environment stats("package:stats");
     Function ts = stats["ts"];
@@ -88,7 +96,7 @@ NumericMatrix create_ts(NumericMatrix x, int first, int last, int freq,
     end[0] = last / freq;
     end[1] = last % freq + 1;
 
-    // 
+    //
     CharacterVector classes(4);
     classes[0] = "regts";
     classes[1] = "mts";
