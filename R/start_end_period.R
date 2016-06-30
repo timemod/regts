980e5d15b04e3826d19c53df470127d1a1cb6187
Rob van Harrevelt <rvh.cpb.nl>
2016-06-30 11:49:56 +0200

    Replaced expect_identical by expect_equal in some tests in tests/testthat/test_regts.R.

--- R/start_end_period.R
+++ R/start_end_period.R
@@ -1,6 +1,6 @@
 #' Returns the start period of a \link{regts} or a \link{regperiod_range}
 #'
-#' @param x  a \link{regts} or \link{regperiod_range{ object}}
+#' @param x  a \link{regts} or \link{regperiod_range} object
 #' @return a \link{regperiod} object representing the first period of the
 #' range. The return value can be \code{NULL} if argument \code{x} is a
 #' \code{regperiod_range} with no lower boundary.
@@ -11,7 +11,7 @@ start_period <- function(x) {
 
 #' Returns the end period of a \link{regts} or a \link{regperiod_range}
 #'
-#' @param x  a \link{regts} or \link{regperiod_range{ object}}
+#' @param x  a \link{regts} or \link{regperiod_range} object
 #' @return a \link{regperiod} object representing the last period of the
 #' range. The return value can be \code{NULL} if argument \code{x} is a
 #' \code{regperiod_range} with no upper boundary.
