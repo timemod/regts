cabbd764b0f44606a768fa6bd88ef9e64aeee14f
Rob van Harrevelt <rvh@cpb.nl>
2016-07-25 14:47:31 +0200

    Function filter_columns renamed to select_columns. Improved the documentation of this function.

--- R/utils.R
+++ R/utils.R
@@ -1,20 +1,24 @@
-#' Select columns using a filter
+#' Select columns using a regular expression
 #'
-#' This function selects columns of an R object that has columns names
-#' (e.g. a \code{data.frame}, \code{matrix}, \code{ts} or
-#' \code{regts})  using a
-#' regular expression.
-#' @param x any R object with column names (\code{data.frame},
-#' \code{matrix}, \code{ts}, \code{regts})
+#' This function selects columns of an R object with column names
+#' (for example a \code{\link{data.frame}}, \code{\link{matrix}} or
+#' \code{\link[stats]{ts}} or \code{\link{regts}}).
+#' The columns with names matching a given regular expression are selected.
+#' @param x an R object with column names (e.g. a \code{data.frame},
+#' \code{matrix}, \code{ts} or \code{regts})
 #' @param regex a regular expression used to select column
 #' @param drop if \code{TRUE}, the result is coerced to a vector if possible.
-#' @return the selection of object \code{x}
+#' @return the column selection of object \code{x}
+#' @examples
+#' data <- regts(matrix(1:20, ncol = 4), start = "2010Q2",
+#'               names = c("nlc", "ukc", "nly", "uky"))
+#' nl_data <- select_columns(data, "nl.*")
 #' @export
-filter_columns <- function(x, regex, drop = TRUE) {
+select_columns <- function(x, regex, drop = TRUE) {
     cnames <- colnames(x)
     if (is.null(cnames)) {
         stop("No column names available. No selection possible")
     }
-    sel <- grep(regex, colnames(x))
+    sel <- grep(regex, cnames)
     return (x[ , sel, drop = drop])
 }
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,46 +1,46 @@
 context("utils")
 
-test_that("filter_columns, univariate timseries", {
+test_that("select_columns, univariate timseries", {
     df <- data.frame(period = c("2015Q3", "2015Q4", "2016Q1"), a = 1:3,
                      stringsAsFactors = FALSE)
-    expect_identical(filter_columns(df, 'a'), df[, 'a'])
+    expect_identical(select_columns(df, 'a'), df[, 'a'])
     ts1 <- regts(df[, 2, drop = FALSE] , start = "2015Q3")
-    expect_identical(filter_columns(df, 'a'), df[, 'a'])
-    expect_identical(filter_columns(df, 'x'), df[, character(0)])
-    expect_identical(filter_columns(ts1, 'a'), ts1[, 1])
-    expect_identical(filter_columns(ts1, 'x'), ts1[, character(0)])
+    expect_identical(select_columns(df, 'a'), df[, 'a'])
+    expect_identical(select_columns(df, 'x'), df[, character(0)])
+    expect_identical(select_columns(ts1, 'a'), ts1[, 1])
+    expect_identical(select_columns(ts1, 'x'), ts1[, character(0)])
 
-    expect_identical(filter_columns(df, 'a', drop = FALSE),
+    expect_identical(select_columns(df, 'a', drop = FALSE),
                      df[, 'a', drop = FALSE])
-    expect_identical(filter_columns(df, 'x', drop = FALSE),
+    expect_identical(select_columns(df, 'x', drop = FALSE),
                      df[, character(0), drop = FALSE])
-    expect_identical(filter_columns(ts1, 'a', drop = FALSE),
+    expect_identical(select_columns(ts1, 'a', drop = FALSE),
                      ts1[, 1, drop = FALSE])
-    expect_identical(filter_columns(ts1, 'x', drop = FALSE),
+    expect_identical(select_columns(ts1, 'x', drop = FALSE),
                      ts1[, character(0), drop = FALSE])
 
     # no colnames
     ts2 <- regts(df[, 2] , start = "2015Q3")
-    expect_error(filter_columns(ts2, 'x'),
+    expect_error(select_columns(ts2, 'x'),
                  "No column names available. No selection possible")
 })
 
-test_that("filter_columns, multivariate timseries", {
+test_that("select_columns, multivariate timseries", {
     df <- data.frame(period = c("2015Q3", "2015Q4", "2016Q1"), a = 1:3,
                      b = 10:12, stringsAsFactors = FALSE)
     ts1 <- as.regts(df, time_column = 1)
-    expect_identical(filter_columns(df, 'b.*'), df[, 'b'])
-    expect_identical(filter_columns(df, 'x.*'), df[, character(0)])
-    expect_identical(filter_columns(ts1, 'b.*'), ts1[, 'b'])
-    expect_identical(filter_columns(ts1, 'x.*'), ts1[, character(0)])
+    expect_identical(select_columns(df, 'b.*'), df[, 'b'])
+    expect_identical(select_columns(df, 'x.*'), df[, character(0)])
+    expect_identical(select_columns(ts1, 'b.*'), ts1[, 'b'])
+    expect_identical(select_columns(ts1, 'x.*'), ts1[, character(0)])
 
-    expect_identical(filter_columns(df, 'b.*', drop = FALSE), df[, 'b',
+    expect_identical(select_columns(df, 'b.*', drop = FALSE), df[, 'b',
                                                                  drop = FALSE])
-    expect_identical(filter_columns(df, 'x.*', drop = FALSE),
+    expect_identical(select_columns(df, 'x.*', drop = FALSE),
                      df[, character(0), drop = FALSE])
-    expect_identical(filter_columns(ts1, 'b.*', drop = FALSE),
+    expect_identical(select_columns(ts1, 'b.*', drop = FALSE),
                      ts1[, 'b', drop = FALSE])
-    expect_identical(filter_columns(ts1, 'x.*', drop = FALSE),
+    expect_identical(select_columns(ts1, 'x.*', drop = FALSE),
                      ts1[, character(0), drop = FALSE])
 })
 
