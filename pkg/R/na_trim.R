5ce690f82124f749f9aede36ce6cfaeedb7a2ca7
Anita van der Roest <J.C.van.der.Roest@cpb.nl>
2017-01-11 17:16:52 +0100

    Updated and renamed files, arguments and tests for na_trim function. Labels are preserved now.

--- pkg/R/na_trim.R
+++ pkg/R/na_trim.R
@@ -1,15 +1,15 @@
 #' Function for removing leading and trailing NAs
 #'
-#' This function removes leading or trailing Nas or both from a (multivariate)
+#' This function removes leading or trailing NAs or both from a (multivariate)
 #' regts object.
-#' For multivariate regts a row will default be regarded as NA if all elements
+#' For multivariate regts a row will by default be regarded as NA if all elements
 #' in the row are NA.  Specify \code{"any"} to change this behaviour.
 #'
 #' @param x a regts object
-#' @param sides character with values \code{"both",} \code{"left"} or
-#' \code{"right"} to remove NAs from
-#' both sides (the default), just from the left side or just from the right side.
-#' @param is.na character with values \code{"all"} or \code{"any"}. If \code{"all"}
+#' @param method character with values \code{"both",} \code{"first"} or
+#' \code{"last"} to remove NAs at both ends (by default), just at the start
+#' or just at the end.
+#' @param is_na character with values \code{"all"} or \code{"any"}. If \code{"all"}
 #' (default) then a row will be regarded as \code{NA} only if all elements in
 #' the row are \code{NA}. If \code{"any"} then a row will be regarded as \code{NA}
 #' if it has any \code{NA}s. For one dimensional regts objects this argument
@@ -19,29 +19,29 @@
 #' @examples
 #' # remove only leading NAs
 #' ts1 <- regts(c(NA,1,3,NA,4,8,NA), start = "2000")
-#' na_skip(ts1, sides = "left")
+#' na_trim(ts1, method = "first")
 #'
 #' # remove trailing NAs if all elements in the row are NA
 #' data <- matrix(c(1,3,NA,2,5,NA,3,7,NA), ncol = 3)
 #' rts <- regts(data, start = "2010Q2", names = c("a", "b", "c"))
-#' na_skip(rts, sides = "right")
+#' na_trim(rts, method = "last")
 #'
 #' data <- matrix(c(NA,3,NA,NA,5,6,NA,7,NA), ncol = 3)
 #' rts <- regts(data, start = "2010Q1", names = c("a", "b", "c"))
-#' # remove leading/trailing NAs if all elements in the row are NA
-#' na_skip(rts)
-#' # or remove NAs if any NA occurs in that row
-#' na_skip(rts, is.na = "any")
+#' # remove leading NAs if all elements in the row are NA
+#' na_trim(rts, method = "first")
+#' # or remove rows NAs if any NA occurs in that row
+#' na_trim(rts, method =is_na = "any")
 
 #' @export
-na_skip <- function (x, sides = c("both", "left","right"),
-                     is.na = c("all", "any")) {
+na_trim <- function (x, method = c("both", "first","last"),
+                     is_na = c("all", "any")) {
 
     if (!is.ts(x)) {
         stop("Argument x is not a timeseries")
     }
-    side <- match.arg(sides)
-    isna <- match.arg(is.na)
+    side <- match.arg(method)
+    isna <- match.arg(is_na)
 
     if (!is.matrix(x)) {
         elem <- which(!is.na(x))
@@ -54,19 +54,19 @@ na_skip <- function (x, sides = c("both", "left","right"),
         }
         len <- nrow(x)
     }
+    if (length(elem) == 0) {
+        return(NULL)
+    }
     if (side == "both") {
         sel <- min(elem) : max(elem)
-    } else if (side == "left") {
+    } else if (side == "first") {
         sel <- min(elem) : len
     } else {
         sel <- 1 : max(elem)
     }
     per <- start_period(get_regperiod_range(x)) + sel[1] - 1
-    if (!is.matrix(x)) {
-        return(regts(x[sel], start = per))
-    } else {
-        return(regts(x[sel, ], start = per))
-    }
+    period <- regperiod_range(per, per + length(sel) - 1)
+
+    return(x[period,])
+
 }
-
-
---
+++ pkg/tests/testthat/test_na_trim.R
@@ -0,0 +1,74 @@
+context("na_trim")
+
+test_that("univariate timeseries", {
+    ts1 <- regts(c(NA,1,3,NA,4,8), start = "2000")
+    ts2 <- regts(c(NA,1,3,NA,4,8,NA), start = "2000")
+    expect_identical(ts1, na_trim(ts2, method = "last"))
+    expect_identical(na_trim(ts1, method = "first"), na_trim(ts2))
+    expect_identical(na_trim(ts2), na_trim(ts2, method = "both"))
+})
+
+test_that("multivariate timeseries, is_na = all", {
+    dataNA <- matrix(rep(NA, 15), ncol = 3)
+    rtsNA <- regts(dataNA, start = "2017Q1", names = c("a", "b", "c"))
+
+    data <- matrix(1:6, ncol = 3)
+    period <- regperiod_range("2017Q2/2017Q3")
+    rts <- regts(data, period = period, names = c("a", "b", "c"))
+
+    rtsNA[period] <- rts
+    rts1 <- rtsNA["2017Q1/2017Q3"]
+    rts2 <- rtsNA["2017Q2/2018Q1"]
+
+    # remove leading/trailing/all NAs
+    expect_identical(na_trim(rtsNA, method = "both"), rts)
+    expect_identical(na_trim(rtsNA, method = "last"), rts1)
+    expect_identical(na_trim(rtsNA, method = "first"), rts2)
+
+    expect_identical(na_trim(rts, method = "last"), na_trim(rts, is_na = "all"))
+})
+
+test_that("multivariate timeseries, is_na = any", {
+    dataNA <- matrix(rep(NA, 12), ncol = 3)
+    rtsNA <- regts(dataNA, start = "2017Q1", names = c("a", "b", "c"))
+
+    data <- matrix(c(1,2,NA,3,4,NA,NA,6,7), ncol = 3)
+    period <- regperiod_range("2017Q1/2017Q3")
+    rts <- regts(data, period = period, names = c("a", "b", "c"))
+
+    rtsNA[period] <- rts
+    rts1 <- rtsNA["2017Q2/2017Q4"]
+    rts2 <- rtsNA["2017Q2/2017Q2"]
+
+    # remove NAs if any element in the row is NA
+    expect_identical(na_trim(rtsNA, method = "first", is_na = "any"), rts1)
+    expect_identical(na_trim(rtsNA, is_na = "any"), na_trim(rts2))
+
+    expect_identical(na_trim(rts1, is_na = "any"), na_trim(rts2, is_na = "any"))
+    expect_identical(na_trim(rts[, c("b","c")], is_na = "any", method = "both"),
+                     na_trim(rts1[, c("b","c")], is_na = "any", method = "last"))
+})
+
+test_that("preserve labels", {
+
+    ts <- regts(c(NA,1,3,NA,4,8), start = "2000", labels = "tijdreeks")
+    expect_identical(ts_labels(ts), ts_labels(na_trim(ts)))
+
+    dataNA <- matrix(rep(NA, 12), ncol = 3)
+    rtsNA <- regts(dataNA, start = "2017Q1", names = c("a", "b", "c"),
+                   labels = c("label_a", "label_b", "label_c"))
+
+    data <- matrix(c(1,2,NA,3,4,NA,NA,6,NA), ncol = 3)
+    period <- regperiod_range("2017Q1/2017Q3")
+    rts <- regts(data, period = period, names = c("a", "b", "c"),
+                 labels = c("label_a", "label_b", "label_c"))
+
+    rts1 <- na_trim(rts)
+    expect_identical(ts_labels(rts), ts_labels(rts1))
+
+    rts2 <- na_trim(rts, method = "last")
+    expect_identical(ts_labels(rts), ts_labels(rts2))
+
+    rts3 <- na_trim(rts, is_na = "any")
+    expect_identical(ts_labels(rts), ts_labels(rts3))
+})
