6f15c8f0f17f63239aaee4a1fc675b6fb55b61d2
Rob van Harrevelt <rvh@cpb.nl>
2017-03-24 10:42:09 +0100

    Updated examples

--- examples/read_ts/example1.R
+++ examples/read_ts/example1.R
@@ -2,11 +2,21 @@ library(regts)
 
 csv_file <- "csv/example1.csv"
 
+
+# read the timeseries and skip the first row (employing argument skip)
 df <- read.csv(csv_file, check.names = FALSE, stringsAsFactors = FALSE,
                skip = 1)
+
+# skip the first
+df <- df[, -1]
+
 ts1 <- read_ts(df)
 print(ts1)
 
 ts2 <- read_ts(df, labels = "after")
 print(ts2)
 View(ts2)
+
+ts3 <- read_ts(df, labels = "before")
+print(ts3)
+View(ts3)
--- examples/read_ts/example2.R
+++ examples/read_ts/example2.R
@@ -4,9 +4,16 @@ csv_file <- "csv/example2.csv"
 
 df <- read.csv(csv_file, check.names = FALSE, stringsAsFactors = FALSE)
 
+# skip the first column
+df <- df[, -1]
+
 ts1 <- read_ts(df)
 print(ts1)
 
 ts2 <- read_ts(df, labels = "after")
 print(ts2)
 View(ts2)
+
+ts3 <- read_ts(df, labels = "before")
+print(ts3)
+View(ts3)
