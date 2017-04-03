2cc49806eb8322ace8ed12936b813eb93e724e0b
Rob van Harrevel <rvanharrevelt@gmail.com>
2017-04-03 17:12:40 +0200

    Added a test of function read_ts_csv for some Taxus input files. These tests did not run correctly with the previous version.

--- pkg/tests/testthat/read_ts/test_read_ts_xlsx.R
+++ pkg/tests/testthat/read_ts/test_read_ts_xlsx.R
@@ -15,41 +15,41 @@ ts_labels(correct_result_labels) <- c("Timeseries a", "Timeseries b (EUR)")
 
 test_that("example1.xlsx is read correctly",  {
 
-	xlsx_file <- "xlsx/example1.xlsx"
+  xlsx_file <- "xlsx/example1.xlsx"
 
-    result <- read_ts_xlsx(xlsx_file, skiprow = 1)
-	expect_identical(result, correct_result)
+  result <- read_ts_xlsx(xlsx_file, skiprow = 1)
+  expect_identical(result, correct_result)
 
-    result2 <- read_ts_xlsx(xlsx_file, skiprow = 1, labels = "after")
-	expect_identical(result2, correct_result_labels)
+  result2 <- read_ts_xlsx(xlsx_file, skiprow = 1, labels = "after")
+  expect_identical(result2, correct_result_labels)
 
-    correct_result_labels2 <- correct_result[ , "b", drop = FALSE]
-    colnames(correct_result_labels2) <- "(EUR)"
-    ts_labels(correct_result_labels2) <- "b Timeseries b"
+  correct_result_labels2 <- correct_result[ , "b", drop = FALSE]
+  colnames(correct_result_labels2) <- "(EUR)"
+  ts_labels(correct_result_labels2) <- "b Timeseries b"
 
-    result3 <- read_ts_xlsx(xlsx_file, skiprow = 1, labels = "before")
-	expect_identical(result3, correct_result_labels2)
+  result3 <- read_ts_xlsx(xlsx_file, skiprow = 1, labels = "before")
+  expect_identical(result3, correct_result_labels2)
 })
 
 test_that("example2.xlsx is read correctly",  {
 
-	xlsx_file <- "xlsx/example2.xlsx"
+  xlsx_file <- "xlsx/example2.xlsx"
 
-    result <- read_ts_xlsx(xlsx_file, skipcol = 1, sheet = 2)
-	expect_identical(result, correct_result)
+  result <- read_ts_xlsx(xlsx_file, skipcol = 1, sheet = 2)
+  expect_identical(result, correct_result)
 
-    result2 <- read_ts_xlsx(xlsx_file, sheet = "example2", 
-                            skipcol = 1, labels = "after")
-	expect_identical(result2, correct_result_labels)
+  result2 <- read_ts_xlsx(xlsx_file, sheet = "example2",
+                          skipcol = 1, labels = "after")
+  expect_identical(result2, correct_result_labels)
 
-    correct_result_labels2 <- correct_result[ , "b", drop = FALSE]
-    colnames(correct_result_labels2) <- "(EUR)"
-    correct_result_labels2[, "Model Taxus ran for the CEP 2017"] <- NA
-    correct_result_labels2 <- correct_result_labels2[, c(2,1)]
-    ts_labels(correct_result_labels2) <- c("", "b Timeseries b")
+  correct_result_labels2 <- correct_result[ , "b", drop = FALSE]
+  colnames(correct_result_labels2) <- "(EUR)"
+  correct_result_labels2[, "Model Taxus ran for the CEP 2017"] <- NA
+  correct_result_labels2 <- correct_result_labels2[, c(2,1)]
+  ts_labels(correct_result_labels2) <- c("", "b Timeseries b")
 
-    result3 <- read_ts_xlsx(xlsx_file, skipcol = 1, sheet = 2,
-                            labels = "before")
+  result3 <- read_ts_xlsx(xlsx_file, skipcol = 1, sheet = 2,
+                          labels = "before")
 
-	expect_identical(result3, correct_result_labels2)
+  expect_identical(result3, correct_result_labels2)
 })
