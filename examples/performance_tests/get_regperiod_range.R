8a44cb681049ae708791cc71b7c80c83d232b6d3
Anita van der Roest <J.C.van.der.Roest@cpb.nl>
2017-04-10 16:52:26 +0200

    Updated directory examples for the function renaming in package regts. Most significant change: regperiod -> period

--- examples/performance_tests/get_regperiod_range.R
+++ examples/performance_tests/get_regperiod_range.R
@@ -1,10 +1,10 @@
-# test the performance of get_regperiod_range and a number of alternatives.
+# test the performance of get_period_range and a number of alternatives.
 library(regts)
 source("tests/performance_tests/time_commands.R")
 
 regts1 <- regts(as.numeric(1:30), start = "2010Q2")
 
-get_regperiod_range_alt1 <- function(x) {
+get_period_range_alt1 <- function(x) {
     if (!inherits(x, 'ts')) {
         stop("Argument x is not a timeseries")
     }
@@ -12,10 +12,10 @@ get_regperiod_range_alt1 <- function(x) {
     freq <- frequency(x)
     first <- start[0] * freq + start[1] - 1;
     last  <- first + nrow(x) - 1;
-    return (structure(c(first, last, freq), class = "regperiod_range"))
+    return (structure(c(first, last, freq), class = "period_range"))
 }
 
-get_regperiod_range_alt2 <- function(x) {
+get_period_range_alt2 <- function(x) {
     if (!inherits(x, 'ts')) {
         stop("Argument x is not a timeseries")
     }
@@ -25,12 +25,12 @@ get_regperiod_range_alt2 <- function(x) {
     subp <- round((tsp[1] - year ) * frequency);
     first <- year * frequency + subp;
     last  <- first + nrow(x) - 1;
-    return (structure(c(first, last, frequency), class = "regperiod_range"))
+    return (structure(c(first, last, frequency), class = "period_range"))
 }
 
-commands <- c("get_regperiod_range(regts1)",
-              "get_regperiod_range_alt1(regts1)",
-              "get_regperiod_range_alt2(regts1)"
+commands <- c("get_period_range(regts1)",
+              "get_period_range_alt1(regts1)",
+              "get_period_range_alt2(regts1)"
 )
 
 print(time_commands(commands))
--- examples/performance_tests/period_selection.R
+++ examples/performance_tests/period_selection.R
@@ -7,7 +7,7 @@ regts1 <- regts(matrix(as.numeric(1:30), ncol = 1), start = "2010Q2")
 ts1 <- regts:::unregts(regts1)
 zoo1 <- as.zooreg(ts1)
 
-p1 <- as.regperiod_range("2010Q4/2011Q1")
+p1 <- as.period_range("2010Q4/2011Q1")
 
 # start and end period for zoo
 startp <- as.yearqtr(start_period(p1))
