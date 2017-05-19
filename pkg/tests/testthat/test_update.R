b784225d14bb7c54e6792e3928d5d3470acbd727
Anita van der Roest <J.C.van.der.Roest@cpb.nl>
2017-05-19 14:06:14 +0200

    Testjob for update function

--- pkg/tests/testthat/test_update.R
+++ pkg/tests/testthat/test_update.R
@@ -7,18 +7,13 @@ test_that("head, tail & topleft for regts", {
 	x2 <- regts(matrix(data = rep(1), nc = 3), period = "2000/2001",
             names = c("a", "c", "d"))
 
-	r1 <- tsupdate(x1, x2, "replace")		
+	r1 <- tsupdate(x1, x2, "replace")
 	r2 <- tsupdate(x2, x1, "replace")
-	
-	u1 <- tsupdate(x1, x2, "tsupd")		
-	u2 <- tsupdate(x2, x1, "tsupd")	
+
+	u1 <- tsupdate(x1, x2, "tsupd")
+	u2 <- tsupdate(x2, x1, "tsupd")
 
 # NA's toevoegen
 
- ...
- 
-  expect_identical(topleft(data), head(data[, 1:10 ]))
-  expect_identical(topleft(data), tail(data[period1, 1:10 ]))
-  expect_identical(topleft(data, ncol = 6), head(data[, 1:6 ]))
-  expect_identical(topleft(data, ncol = 5), topleft(data2, ncol = 5))
+
 })
