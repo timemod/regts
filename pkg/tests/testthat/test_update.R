8c13b9327dcef1cd43e8684ecaf23a5c129b7a73
Anita van der Roest <J.C.van.der.Roest@cpb.nl>
2017-05-19 14:59:56 +0200

    Testjob updated

--- pkg/tests/testthat/test_update.R
+++ pkg/tests/testthat/test_update.R
@@ -1,10 +1,10 @@
-context("head, tail & topleft")
+context("update regts")
 
-test_that("head, tail & topleft for regts", {
+test_that("replace, tsupd", {
 
-    x1 <- regts(matrix(data = rep(1), nc = 3), period = "2000/2002",
+  x1 <- regts(matrix(data = rep(1), nc = 3), period = "2000/2002",
             names = c("a", "b", "c"))
-	x2 <- regts(matrix(data = rep(1), nc = 3), period = "2000/2001",
+	x2 <- regts(matrix(data = rep(2), nc = 3), period = "2000/2001",
             names = c("a", "c", "d"))
 
 	r1 <- tsupdate(x1, x2, "replace")
@@ -14,6 +14,12 @@ test_that("head, tail & topleft for regts", {
 	u2 <- tsupdate(x2, x1, "tsupd")
 
 # NA's toevoegen
+	xNa1 <- x1
+	xNa1["2000", 1] <- NA
+	xNa2 <- x2
+	xNa2["2000/2001", 2]  <- NA
 
+	n1 <- tsupdate(xNa1, xNa2, "tsupdna")
+	n2 <- tsupdate(xNa2, xNa1, "tsupdna")
 
 })
