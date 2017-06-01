113a29c22c7ef8e05d1f98089cf76c92df7c1217
Rob van Harrevelt <rvh@cpb.nl>
2017-06-01 15:42:17 +0200

    The function t (transpose) now returns a matrix, also  for regts objects.

---
+++ pkg/tests/testthat/test_t.R
@@ -0,0 +1,15 @@
+context("t.regts")
+
+test_that("t.regts for univariate timeseries", {
+  data <- 1:4
+  rts1 <- regts(data, start = "2010Q2")
+  expect_identical(t(rts1), matrix(data, nrow = 1))
+})
+
+test_that("t.regts for univariate timeseries", {
+  data <- matrix(1:4, ncol = 2)
+  colnames(data) <- c("a", "b")
+  rts1 <- regts(data, start = "2010Q2")
+  expect_identical(t(rts1), t(data))
+})
+
