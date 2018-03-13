90ea857b032bc665b63ead2baf777e2417ffe3f0
Rob van Harrevelt <rvanharrevelt@gmail.com>
2018-03-13 18:04:04 +0100

    Argument base_period renamed to base.

--- pkg/R/index_ts.R
+++ pkg/R/index_ts.R
@@ -1,62 +1,77 @@
 #' Construct an index timeseries by scaling
 #'
-#' This function scales a timeseries by dividing all observations with
+#' This function scales a timeseries by dividing all observations by
 #' one selected observation or by the mean of a range of observations.
 #'
 #' @param x  a \code{\link[stats]{ts}} of \code{\link{regts}} object
-#' @param base_period The base period: a \code{\link{period}} or a
-#' \code{\link{period_range}}, or an object that can be coerced to a period or
-#' period range. By default base period is the first period of the input
-#' timeseries.
-#' @param  index_value the index value (by default 100). If \code{base_period}\
-#' is a single period, then the returned timeseries will be equal to the index value
-#' at this period. If \code{base_period} is a period
-#' range with multiple periods, then the average of the index timeseries
-#' is equal to the index value
-#' @return  an index timeseries
+#' @param base a \code{\link{period}} or a
+#' \code{\link{period_range}} specifying the base period
+#' used for calculating the index factor,
+#' or an object that can be coerced to a
+#' \code{period} or \code{period_range}. By default the
+#' base period is the first period of the input timeseries.
+#' @param  index_value the index value (by default 100).
+#' This is the value of the index timeseries at the base period.
+#' If the base period is a \code{period_range} spanning multiple
+#' periods, then \code{index_value} is the average of the index timeseries
+#' at the base period.
+#' @examples
+#' \dontshow{
+#' set.seed(123)
+#' }
+#' ab <- regts(matrix(rnorm(18), ncol = 2), start = "2016Q1",
+#'             names = c("a", "b"))
+#'
+#' index_ts(ab)
+#'
+#' index_ts(ab, base = "2017", index_value = 1)
+#'
 #' @export
-index_ts <- function(x, base_period = start_period(get_period_range(x)),
-                     index_value = 100) {
+index_ts <- function(x, base = start_period(x), index_value = 100) {
 
   if (!is.ts(x)) {
     stop("Argument x is not a timeseries")
   }
 
-  base_period <- as.period_range(base_period)
-  if (!missing(base_period)) {
-    freq_base <- frequency(base_period)
+  if (!missing(base)) {
+    base <- as.period_range(base)
+    freq_base <- frequency(base)
     freq_x <- frequency(x)
     if (freq_base != freq_x) {
       if (freq_base > freq_x) {
-        stop(paste("Argument base_period should not have a higher frequency",
+        stop(paste("Argument period should not have a higher frequency",
                    "than the input timeseries"))
       }
-      base_period <- change_frequency(base_period, new_frequency = freq_x)
+      base <- change_frequency(base, new_frequency = freq_x)
     }
+  } else {
+    base <- period_range(base, base)
   }
 
   p_x <- get_period_range(x)
-  startp_x <- start_period(p_x)
-  endp_x <- end_period(p_x)
+  startp_x <- p_x[1]
+  endp_x <- p_x[2]
 
-  startp_base <- start_period(base_period)
-  endp_base <- end_period(base_period)
+  startp_base <- base[1]
+  endp_base <- base[2]
+  if (is.na(startp_base) || is.na(endp_base))
+    {
+    stop("The base period should have a lower and upper bound")
+  }
 
   if (startp_base < startp_x || endp_base > endp_x) {
-    # TODO: improve error message
-    stop(paste0("Base period (", base_period, " ) not within timeseries period",
+    stop(paste0("Base period (", base, ") not within timeseries period",
                 " (",  p_x, ")"))
   }
 
+  psel <- (startp_base : endp_base) - startp_x + 1
+
   if (is.mts(x)) {
     # multivariate timeseries
-    i1 <- startp_base - startp_x + 1
-    i2 <- endp_base - startp_x+ 1
-    x[] <-  apply(x, FUN = function(x) {index_value * x / mean(x[i1:i2])},
-                  MARGIN = 2)
-    return(x)
+    x[] <- apply(x, FUN = function(x) {x / mean(x[psel])}, MARGIN = 2)
+    return(x * index_value)
   } else {
     # univariate timeseries
-    return(index_value * x / mean(x[base_period]))
+    return(index_value * x / mean(x[psel]))
   }
 }
--- pkg/tests/testthat/test_index_ts.R
+++ pkg/tests/testthat/test_index_ts.R
@@ -16,23 +16,23 @@ test_that("default arguments", {
   expect_identical(index_ts(ab), expected_result)
 })
 
-test_that("base_period and index_value", {
+test_that("base and index_value", {
   bp <- "2021Q1"
-  expect_equal(index_ts(a, base_period = bp), (100 / 13)* a)
+  expect_equal(index_ts(a, base = bp), (100 / 13)* a)
   expected_result  <- (100 / 13) * ab
   expected_result$b <- expected_result$a
-  expect_identical(index_ts(ab, bp), expected_result)
+  expect_equal(index_ts(ab, bp), expected_result)
 
   bp <- "2020"
   mean_2020 <- mean(a[bp])
-  expect_equal(index_ts(a, base_period = bp), (100 / mean_2020) * a)
+  expect_equal(index_ts(a, base = bp), (100 / mean_2020) * a)
   expected_result  <- (100 / mean_2020) * ab
   expected_result$b <- expected_result$a
   expect_equal(index_ts(ab, bp), expected_result)
 
   bp <- period_range("2019Q2", "2020Q1")
   mean_bp <- mean(a[bp])
-  expect_equal(index_ts(a, base_period = bp,
+  expect_equal(index_ts(a, base = bp,
                         index_value = 1), (1 / mean_bp) * a)
   expected_result  <- (1 / mean_bp) * ab
   expected_result$b <- expected_result$a
@@ -40,6 +40,19 @@ test_that("base_period and index_value", {
 })
 
 test_that("errors", {
-  msg <- "Base period \\(2017Q1 \\) not within timeseries period \\(2018Q1/2022Q2\\)"
-  expect_error(index_ts(a, base_period  = "2017Q1"), msg)
+  msg <- "Base period \\(2017Q1\\) not within timeseries period \\(2018Q1/2022Q2\\)"
+  expect_error(index_ts(a, base  = "2017Q1"), msg)
 })
+
+
+test_that("NA values", {
+  a_NA <- a
+  a_NA["2019Q3"] <- NA
+  index_ts(a_NA,  base = "2019Q3/2019Q4")
+  ab_NA <- ab
+  expect_identical(index_ts(a), 100 * a)
+  expected_result  <- 100 * ab
+  expected_result$b <- expected_result$a
+  expect_identical(index_ts(ab), expected_result)
+})
+
