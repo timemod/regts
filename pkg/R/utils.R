cd44aedb3a3d558af7e0f748dba91bcf0f4126b7
Rob van Harrevelt <rvanharrevelt@gmail.com>
2018-04-09 18:15:47 +0200

    It is now possible to pass arguments to function grep via function select_columns.

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -4,10 +4,12 @@
 #' (for example a \code{\link[base]{data.frame}}, \code{\link[base]{matrix}},
 #' \code{\link[stats]{ts}} or \code{\link{regts}}).
 #' The columns with names matching a given regular expression are selected.
+#' This function employs base R function \code{\link{grep}}
 #' @param x an R object with column names (e.g. a \code{data.frame},
 #' \code{matrix}, \code{ts} or \code{regts})
 #' @param regex a regular expression used to select a column
-#' @param drop if \code{TRUE}, the result is coerced to a vector if possible.
+#' @param drop if \code{TRUE}, the result is coerced to a vector if possible
+#' @param ... arguments passed to \link{grep}
 #' @return the column selection of object \code{x}
 #' @examples
 #' data <- regts(matrix(1:20, ncol = 4), start = "2010Q2",
@@ -16,12 +18,12 @@
 #' # select all columns with names starting with nl
 #' nl_data <- select_columns(data, "nl.*")
 #' @export
-select_columns <- function(x, regex, drop = TRUE) {
+select_columns <- function(x, regex, drop = TRUE, ...) {
   cnames <- colnames(x)
   if (is.null(cnames)) {
     stop("No column names available. No selection possible")
   }
-  sel <- grep(regex, cnames)
+  sel <- grep(regex, cnames, ...)
   return (x[ , sel, drop = drop])
 }
 
