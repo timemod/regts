68c02c6548c704dbf4b68dba6bf1f5d0034c7e2d
Rob van Harrevelt <rvanharrevelt@gmail.com>
2018-06-15 21:20:14 +0200

    Added some more tests for function rel2index and pct2index.

--- pkg/tests/testthat/test_rel2index.R
+++ pkg/tests/testthat/test_rel2index.R
@@ -40,3 +40,30 @@ test_that("pct2index", {
   ts1_index <- pct2index(ts1_rel, scale = 1)
   expect_equal(ts1, ts1_index)
 })
+
+test_that("NA values", {
+  ts1 <- regts(c(1L, 2L, 3L, NA, 5L, 6L), start = "2010Q2")
+  ts1_rel <- diff(ts1) / abs(lag(ts1, -1))
+  ts1_index <- rel2index(ts1_rel, scale = 1)
+  expected_result <- ts1
+  expected_result["2011Q2/"] <- NA
+  expect_equal(ts1_index, expected_result)
+})
+
+test_that("Inf values", {
+  ts1 <- regts(c(1L, 2L, 3L, Inf, 5L, 6L), start = "2010M2")
+  ts1_rel <- diff(ts1) / abs(lag(ts1, -1))
+  ts1_index <- rel2index(ts1_rel, scale = 1)
+  expected_result <- ts1
+  expected_result["2010m6/"] <- NaN
+  expect_equal(ts1_index, expected_result)
+})
+
+test_that("errors", {
+  ts1 <- regts(c(1, 2, 3), start = "2010Q2")
+  msg <- "The base period should lie between 2010Q1 and 2010Q4."
+  expect_error(rel2index(ts1, base = "2018Q3"), msg)
+  msg <- paste("The base period 2018M03 has a different frequency than the",
+               "timeseries \\(4\\).")
+  expect_error(rel2index(ts1, base = "2018M3"), msg)
+})
