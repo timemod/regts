42ae53ace8f8f49180afa2294d4dfb8457522dfd
Rob van Harrevelt <rvanharrevelt@gmail.com>
2018-07-02 12:05:30 +0200

    Added test for print.regts and printobj.

---
+++ pkg/tests/testthat/test_print.R
@@ -0,0 +1,32 @@
+library(testthat)
+library(regts)
+
+context("regts")
+
+rm(list = ls())
+
+test_that("yearly univariate series", {
+  y_ts <- regts(1:2, start = 2016, labels = "xxx")
+  expect_known_output(print(y_ts), "expected_output/print_y_ts.txt")
+  expect_known_output(print(y_ts["2016"]), "expected_output/print_y_ts_sel.txt")
+  expect_known_output(printobj(y_ts["2016"]),
+                      "expected_output/printobj_y_ts_sel.txt")
+  y_ts2 <- regts(1:20, start = 2016, labels = "xxx")
+  expect_known_output(print(y_ts2), "expected_output/print_y_ts2.txt")
+})
+
+test_that("quarterly series", {
+  q_ts <- regts(1:2, start = "2016Q2", labels = "xxx")
+  expect_known_output(print(q_ts), "expected_output/print_q_ts.txt")
+  q_mts <- cbind(q_ts, q_ts2 = 2 * q_ts)
+  expect_known_output(print(q_mts), "expected_output/print_q_mts.txt")
+  expect_known_output(print(q_mts["2016Q3"]),
+                      "expected_output/print_q_mts_sel.txt")
+})
+
+test_that("frequency 3", {
+  f3_ts <- regts(1:2, start = "2016-2", frequency = 3)
+  expect_known_output(print(f3_ts), "expected_output/print_f3_ts.txt")
+})
+
+
