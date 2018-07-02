e6462667078dc6c3ff15b9d61086446172eac1c8
Rob van Harrevelt <rvanharrevelt@gmail.com>
2018-07-02 17:11:51 +0200

    Function numeric_matrix is no longer exported. It is only used internally in regts.

--- pkg/R/numeric_matrix.R
+++ pkg/R/numeric_matrix.R
@@ -1,30 +1,24 @@
-#' Convert a \code{\link[base]{data.frame}} to a numeric matrix.
-#'
-#' Returns the matrix obtained by converting all the variables in a data frame
-#' to numeric mode and then binding them together as the columns of a matrix.
-#' Factors are converted to characters.
-#'
-#' This function is similar to the function \code{\link[base]{data.matrix}}
-#' of the base package, except that:
-#' * it is possible to specify a decimal separator used to convert strings to
-#' numeric values
-#' * factors are first converted to characters and then to numeric values
-#' * if the data frame contains strings that cannot be converted
-#' to numerical values, then \code{numeric_matrix} gives a warning about
-#' the first 10 character strings that could not be succesfully converted.
-#' @md
-#' @param x a \code{\link[base]{data.frame}}
-#' @param dec decimal separator in number strings. The default is \code{"."}
-#' @examples
-#' df <- data.frame(a = c("1.123", "x", NA), b = c("1", "", "john"),
-#'                  c = 10 : 12)
-#' numeric_matrix(df)
-#' @export
-numeric_matrix <- function(x, dec = ".") {
+# numeric_matrix is an internal function to convert a data.frame to a numeric
+# matrix.
+#
+# It returns the matrix obtained by converting all the variables in a data frame
+# to numeric mode and then binding them together as the columns of a matrix.
+# Factors are converted to characters.
+#
+# This function is similar to the function data.matrix of the base package,
+# except that:
+# * it is possible to specify a decimal separator used to convert strings to
+#   numeric values
+# * factors are first converted to characters and then to numeric values
+# * if the data frame contains strings that cannot be converted
+#   to numerical values, then numeric_matrix gives a warning about
+#   the first 10 character strings that could not be succesfully converted.
+#
+# INPUT
+#   x    a data frame
+#   dec  decimal separator
 
-  if (!is.data.frame(x)) {
-    stop("Argument x is not a data frame")
-  }
+numeric_matrix <- function(x, dec = ".") {
 
   if (nrow(x) == 0 || ncol(x) == 0) {
     # no data available
@@ -39,7 +33,7 @@ numeric_matrix <- function(x, dec = ".") {
                }
   col_names <- colnames(x)
 
-  #  get rid of factors, Dates etc.
+  # convert factors, Dates etc. to characters
   convert_col <- function(x) {
     if (is.numeric(x) | is.character(x) | is.logical(x)) {
       return(x)
@@ -49,7 +43,7 @@ numeric_matrix <- function(x, dec = ".") {
   }
   x <- as.data.frame(lapply(x, FUN = convert_col), stringsAsFactors = FALSE)
 
-  to_numeric_1 <- function(x) {
+  to_numeric <- function(x) {
     if (is.numeric(x)) {
       return(x)
     } else {
@@ -57,7 +51,7 @@ numeric_matrix <- function(x, dec = ".") {
     }
   }
 
-  to_numeric_2 <- function(x) {
+  to_numeric_dec <- function(x) {
     if (is.numeric(x)) {
       return(x)
     } else if (is.character(x)) {
@@ -68,9 +62,9 @@ numeric_matrix <- function(x, dec = ".") {
   }
 
   if (dec != ".") {
-    to_numeric <- to_numeric_2
+    to_numeric <- to_numeric_dec
   } else {
-    to_numeric <- to_numeric_1
+    to_numeric <- to_numeric
   }
 
   warn_msg <- "NAs introduced by coercion"
@@ -85,8 +79,7 @@ numeric_matrix <- function(x, dec = ".") {
   }
 
 
-  convert_cmd <- parse(text = paste("x2 <- sapply(x,",
-                                    "FUN = to_numeric)"))
+  convert_cmd <- parse(text = paste("x2 <- sapply(x,", "FUN = to_numeric)"))
 
   convert_problem <- FALSE
 
@@ -118,7 +111,7 @@ numeric_matrix <- function(x, dec = ".") {
                     paste0(weird_texts, collapse = "\n")))
     } else {
       warning(paste0("NAs introduced by coercion.\n",
-                     nweird, " texts could not be converte to numeric.\n",
+                     nweird, " texts could not be converted to numeric.\n",
                      "The first ", NWEIRD_MAX, " texts that gave problems are:\n",
                      paste0(weird_texts, collapse = "\n")))
     }
