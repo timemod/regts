1e8d4a2dd8a0f529d7824287f706e33c6dc7ea9a
Rob van Harrevelt <rvanharrevelt@gmail.com>
2020-06-17 12:36:20 +0200

    Introduced new function rename_cols.

--- pkg/tests/testthat/test_colname_utils.R
+++ pkg/tests/testthat/test_colname_utils.R
@@ -7,7 +7,7 @@ rm(list = ls())
 
 data <- regts(matrix(1:8, ncol = 2), start = "2010Q2", names = c("a", "b"))
 
-test_that("tag_colnames and change_colnames work correctly", {
+test_that("tag_colnames and change_colnames", {
   data_tagged <- tag_colnames(data, "_input")
   expect_equal(colnames(data_tagged), paste0(colnames(data), "_input"))
 
@@ -24,11 +24,41 @@ test_that("tag_colnames and change_colnames work correctly", {
   expect_equal(colnames(data_tagged_3), paste0(colnames(data), "1"))
 })
 
-
-test_that("errors", {
+test_that("errors for tag_colnames and change_colnames", {
   expect_error(tag_colnames(data, tag = data),
                "Argument 'tag' should be a vector with length 1")
 
   expect_error(change_colnames(data, fun = data),
                "Argument 'fun' is not a function")
 })
+
+
+test_that("rename_cols", {
+  data_renamed <- rename_cols(data, x = a, y = b)
+  expected_result <- data
+  colnames(expected_result) <- c("x", "y")
+  expect_identical(data_renamed, expected_result)
+
+  data_renamed_2 <- rename_cols(data, y = b, p = a,  x = a)
+  expect_identical(data_renamed_2, expected_result)
+
+  data_no_cols <- data
+  colnames(data_no_cols) <- NULL
+  data_renamed_3 <- rename_cols(data_no_cols, y = 2, x  = 1)
+  expect_identical(data_renamed_3, expected_result)
+
+  # duplicate columns
+  data_dupl <- data
+  colnames(data_dupl) <- c("a", "a")
+  data_renamed_4 <- rename_cols(data_dupl, x = "a")
+  colnames(expected_result) <- c("x", "a")
+  expect_identical(data_renamed_4, expected_result)
+})
+
+
+test_that("rename_cols (errors)", {
+  expect_error(rename_cols(data, x = a, y = p))
+  expect_error(rename_cols(data, a))
+  expect_error(rename_cols(data, x = TRUE))
+})
+
