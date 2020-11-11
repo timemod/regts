5117f4c004b73abb9db0f05b2cdb1ab9a6ea3718
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2020-11-11 09:07:00 +0100

    Added a dependency on openxlsx version 4.2.2 (because of argument returnValue of saveWorkbook).

--- examples/write_ts/test1_xlsx.R
+++ examples/write_ts/test1_xlsx.R
@@ -3,8 +3,5 @@ library(regts)
 x <-regts(matrix(1:9, ncol = 3), start = "2010Q2",
           names = c("a", "b", "c"))
 
-library(openxlsx)
-
-write_ts_xlsx2(x, file = "test1.xlsx", sheet_name = "testje",
-               number_format = "#.00")
-#write_ts_xlsx2(x, file = "test1_t.xlsx", rowwise = FALSE)
+write_ts_xlsx(x, file = "test1.xlsx", sheet_name = "testje",
+              number_format = "#.00")
