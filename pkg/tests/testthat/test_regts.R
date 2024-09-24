36aee9a31260901c2b74b64f034527f2948591f1
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2024-09-24 14:29:24 +0200

    Replaced more Rf_error calls by stop calls. Extended the testset.

--- pkg/tests/testthat/test_regts.R
+++ pkg/tests/testthat/test_regts.R
@@ -8,7 +8,7 @@ rm(list = ls())
 test_that("constructor regts for univariate timeseries", {
 
   regts1 <- regts(1:10, start = "2010Q4")
-  ts1 <- ts(1:10, start = c(2010,4), frequency = 4)
+  ts1 <- ts(1:10, start = c(2010, 4), frequency = 4)
   expect_identical(regts1, as.regts(ts1))
   expect_identical(as.ts(regts1), ts1)
   expect_identical(class(regts1), c("regts", "ts"))
@@ -18,15 +18,19 @@ test_that("constructor regts for univariate timeseries", {
 
   r1 <- period_range("2010Q1", "2012Q2")
   regts1 <- regts(matrix(1:10, ncol = 1), period = r1, names = "a")
-  ts1 <- ts(matrix(1:10, ncol = 1), start = c(2010,1), frequency = 4, names = "a")
+  ts1 <- ts(matrix(1:10, ncol = 1), start = c(2010, 1), frequency = 4,
+            names = "a")
   expect_identical(regts1, as.regts(ts1))
   expect_identical(as.ts(regts1), ts1)
 
   regts1 <- regts(1:10, start = "2010", end = "2012")
-  expect_identical(regts1, as.regts(ts(1:10, start = 2010, end = 2012, frequency = 1)))
+  expect_identical(regts1, as.regts(ts(1:10, start = 2010, end = 2012,
+                                       frequency = 1)))
 
-  expect_error(regts(1:10, start = "2010-1"),
-               "Frequency of period 2010-1 unknown. Specify argument frequency.")
+  expect_error(
+    regts(1:10, start = "2010-1"),
+    "Frequency of period 2010-1 unknown. Specify argument frequency."
+  )
   expect_error(regts(1:10, start = "2010-1", period = period_range("2010Q1")),
                "Arguments 'start' and 'period' exclude each other.")
 
@@ -50,7 +54,7 @@ test_that("constructor regts for multivariate time series", {
   data <- matrix(1: 9, ncol = 3)
   colnames(data) <- c("a", "b", "c")
   regts1 <- regts(data, start = "2010Q4")
-  ts1 <- ts(data, start = c(2010,4), frequency = 4)
+  ts1 <- ts(data, start = c(2010, 4), frequency = 4)
   expect_identical(regts1, as.regts(ts1))
   expect_identical(class(regts1), c("regts", "mts", "ts", "matrix", "array"))
   expect_identical(is.regts(regts1), TRUE)
@@ -60,7 +64,7 @@ test_that("constructor regts for multivariate time series", {
   expect_identical(is.matrix(regts1), TRUE)
 
   regts1 <- regts(data, start = "2010M1", end = "2012m3")
-  ts1 <- ts(data, start = c(2010,1), end = c(2012,3), frequency = 12)
+  ts1 <- ts(data, start = c(2010, 1), end = c(2012, 3), frequency = 12)
   expect_identical(regts1, as.regts(ts1))
 })
 
@@ -108,23 +112,23 @@ test_that("period selection in univariate timeseries", {
   regts2 <- regts1["2010Q2/2011Q3"]
   expect_is(regts2, "regts")
   expect_identical(regts2,
-                   as.regts(window(ts1, start = c(2010,2), end = c(2011,3))))
+                   as.regts(window(ts1, start = c(2010, 2), end = c(2011, 3))))
 
   regts2 <- regts1["2008/2012"]
   expect_is(regts2, "regts")
   expect_identical(regts2,
-                   as.regts(window(ts1, start = c(2008,1), end = c(2012,4),
+                   as.regts(window(ts1, start = c(2008, 1), end = c(2012, 4),
                                    extend = TRUE)))
 
   regts2 <- regts1["2008/"]
   expect_is(regts2, "regts")
   expect_identical(regts2,
-                   as.regts(window(ts1, start = c(2008,1), extend = TRUE)))
+                   as.regts(window(ts1, start = c(2008, 1), extend = TRUE)))
 
   regts2 <- regts1["/2011Q2"]
   expect_is(regts2, "regts")
   expect_identical(regts2,
-                   as.regts(window(ts1, end = c(2011,2), extend = TRUE)))
+                   as.regts(window(ts1, end = c(2011, 2), extend = TRUE)))
 
   expect_identical(regts1[3], ts1[3])
   expect_identical(regts1[3:10], ts1[3:10])
@@ -147,31 +151,39 @@ test_that("period selection in univariate timeseries", {
 
 test_that("period / column selection in multivariate timeseries", {
   data <- matrix(1: 9, ncol = 3)
-  ts1 <- ts(data, start = c(2010,4), frequency = 4, names = c("a", "b", "c"))
+  ts1 <- ts(data, start = c(2010, 4), frequency = 4, names = c("a", "b", "c"))
   regts1 <- as.regts(ts1)
 
-  regts2 <- regts1[, 'b']
+  regts2 <- regts1[, "b"]
   expect_is(regts2, "regts")
-  expect_identical(regts2, as.regts(ts1[, 'b']))
+  expect_identical(regts2, as.regts(ts1[, "b"]))
 
-  regts3 <- regts1[, 'b', drop = FALSE]
-  expect_identical(regts3, as.regts(ts1[, 'b', drop = FALSE]))
+  regts3 <- regts1[, "b", drop = FALSE]
+  expect_identical(regts3, as.regts(ts1[, "b", drop = FALSE]))
 
   expect_identical(regts1[, c("b", "a")], as.regts(ts1[, c("b", "a")]))
 
-  expect_identical(regts1['2011Q1', "c"], as.regts(
-    window(ts1[, "c"], start = c(2011,1), end = c(2011,1))))
+  expect_identical(
+    regts1["2011Q1", "c"],
+    as.regts(window(ts1[, "c"], start = c(2011, 1), end = c(2011, 1)))
+  )
 
-  expect_identical(regts1['2011Q1', c("b", "a")], as.regts(
-    window(ts1, start = c(2011,1), end = c(2011,1)))[, c("b", "a"),
-                                                     drop = FALSE])
+  expect_identical(
+    regts1["2011Q1", c("b", "a")],
+    as.regts(window(ts1, start = c(2011, 1), end = c(2011, 1)))[, c("b", "a"),
+                                                                drop = FALSE]
+  )
 
-  expect_identical(regts1['2011'], as.regts(
-    window(ts1, start = c(2011,1), end = c(2011,4), extend = TRUE)))
+  expect_identical(
+    regts1["2011"],
+    as.regts(window(ts1, start = c(2011, 1), end = c(2011, 4), extend = TRUE))
+  )
 
-  expect_identical(regts1['2011Q1', c("a", "b")], as.regts(
-    window(ts1, start = c(2011,1), end = c(2011,1), extend = TRUE)
-    [, c("a", "b"), drop = FALSE]))
+  expect_identical(
+    regts1["2011Q1", c("a", "b")],
+    as.regts(window(ts1, start = c(2011, 1), end = c(2011, 1), extend = TRUE)
+             [, c("a", "b"), drop = FALSE])
+  )
 })
 
 
@@ -182,22 +194,22 @@ test_that("period selection at the lhs of a univariate timeseries", {
 
   # without period extension
   regts2 <- regts1
-  regts2['2010Q2'] <- 2
-  regts2['2011Q3/'] <- 2 * regts1['2011Q3/']
+  regts2["2010Q2"] <- 2
+  regts2["2011Q3/"] <- 2 * regts1["2011Q3/"]
   ts2 <- ts1
-  window(ts2, start = c(2010, 2), end = c(2010,2)) <- 2
+  window(ts2, start = c(2010, 2), end = c(2010, 2)) <- 2
   window(ts2, start = c(2011, 3)) <- 2 * window(ts1, start = c(2011, 3))
   expect_identical(regts2, as.regts(ts2))
 
   # with period extension
   regts2 <- regts1
-  regts2['/2013Q2'] <- 9
-  regts2['2008Q2/2009Q2'] <- regts1['2010Q2/2011Q2']
+  regts2["/2013Q2"] <- 9
+  regts2["2008Q2/2009Q2"] <- regts1["2010Q2/2011Q2"]
   ts2 <- ts1
   suppressWarnings({
-    window(ts2, end = c(2013,2), extend = TRUE) <- 9
+    window(ts2, end = c(2013, 2), extend = TRUE) <- 9
     window(ts2, start = c(2008, 2), end = c(2009, 2), extend = TRUE) <-
-      window(ts1, start = c(2010, 2), end = c(2011,2))
+      window(ts1, start = c(2010, 2), end = c(2011, 2))
   })
   expect_equivalent(regts2, as.regts(ts2))
 
@@ -205,14 +217,14 @@ test_that("period selection at the lhs of a univariate timeseries", {
   regts2 <- regts1
   range1 <- period_range(NULL, "2013Q2")
   regts2[range1] <- 9
-  range2 <- period_range('2008Q2/2009Q2')
-  range3 <- period_range('2010Q2/2011Q2')
+  range2 <- period_range("2008Q2/2009Q2")
+  range3 <- period_range("2010Q2/2011Q2")
   regts2[range2] <- regts1[range3]
   ts2 <- ts1
   suppressWarnings({
-    window(ts2, end = c(2013,2), extend = TRUE) <- 9
+    window(ts2, end = c(2013, 2), extend = TRUE) <- 9
     window(ts2, start = c(2008, 2), end = c(2009, 2), extend = TRUE) <-
-      window(ts1, start = c(2010, 2), end = c(2011,2))
+      window(ts1, start = c(2010, 2), end = c(2011, 2))
   })
   expect_equivalent(regts2, as.regts(ts2))
 })
@@ -221,27 +233,27 @@ test_that("period selection at the lhs of a univariate timeseries", {
 test_that("period and column selection at the lhs of an assignment", {
 
   data <- matrix(1: 10, ncol = 2)
-  ts1 <- ts(data, start = c(2010,4), frequency = 4, names = c("a", "b"))
+  ts1 <- ts(data, start = c(2010, 4), frequency = 4, names = c("a", "b"))
   regts1 <- as.regts(ts1)
 
   regts2 <- regts1
-  regts2['2011Q1/'] <- 2 * regts1['2011Q1/']
-  regts2['/2011Q1', 'a'] <- c(10, 20)
+  regts2["2011Q1/"] <- 2 * regts1["2011Q1/"]
+  regts2["/2011Q1", "a"] <- c(10, 20)
 
   ts2 <- ts1
-  window(ts2, start = c(2011, 1)) <- 2 * window(ts1, start = c(2011,1))
-  window(ts2, end = c(2011, 1))[, 'a'] <- c(10, 20)
+  window(ts2, start = c(2011, 1)) <- 2 * window(ts1, start = c(2011, 1))
+  window(ts2, end = c(2011, 1))[, "a"] <- c(10, 20)
 
   expect_identical(regts2, as.regts(ts2))
 
   regts2 <- regts1
-  regts2[, 'x'] <- 1
-  regts2['2010Q1'] <- 2 * regts2['2011Q1']
-  regts2['2012Q3/2012Q4', 'xx'] <- 2 * regts2['2011Q2/2011Q3', 'x']
-  regts2['2012Q3/',  c('a_2', 'b_2')] <- regts1['2011Q2/2011Q3',
-                                                c('a', 'b')]
-  regts2['2010Q3/',  c('a', 'a_3')] <- 2 * regts1['2011Q2/2011Q3',
-                                                  c('a', 'a')]
+  regts2[, "x"] <- 1
+  regts2["2010Q1"] <- 2 * regts2["2011Q1"]
+  regts2["2012Q3/2012Q4", "xx"] <- 2 * regts2["2011Q2/2011Q3", "x"]
+  regts2["2012Q3/",  c("a_2", "b_2")] <- regts1["2011Q2/2011Q3",
+                                                c("a", "b")]
+  regts2["2010Q3/",  c("a", "a_3")] <- 2 * regts1["2011Q2/2011Q3",
+                                                  c("a", "a")]
   expect_known_value(regts2, "expected_output/period_and_column.rds")
 })
 
@@ -264,7 +276,7 @@ test_that("colnames are preserved in miscellaneous timeseries functions", {
 test_that("several tests for character timeseries", {
   data <- matrix(paste0("text", as.character(1: 10)), ncol = 2)
   regts1 <- regts(data, start = "2010Q4", names = c("a", "b"))
-  ts1 <- ts(data, start = c(2010,4), frequency = 4, names = c("a", "b"))
+  ts1 <- ts(data, start = c(2010, 4), frequency = 4, names = c("a", "b"))
   expect_identical(regts1, as.regts(ts1))
   expect_identical(as.ts(regts1), ts1)
   expect_identical(regts1["2011Q2/2011Q3"],
@@ -272,8 +284,8 @@ test_that("several tests for character timeseries", {
 
   regts2 <- regts1
   regts2["2010Q4"] <- "aap"
-  ts2 <- ts1;
-  window(ts2, start = c(2010, 4), end = c(2010, 4)) <- "aap";
+  ts2 <- ts1
+  window(ts2, start = c(2010, 4), end = c(2010, 4)) <- "aap"
   expect_identical(regts2, as.regts(ts2))
 })
 
@@ -282,7 +294,8 @@ test_that("column selection in a timeseries with 1 row", {
   # result for class ts if drop = TRUE. This behaviour is corrected for
   # in regts (see the implementation of "[.regts")
 
-  regts1 <- regts(matrix(1:3, nc = 3), start = "2010Q2", names = c("a", "b", "c"))
+  regts1 <- regts(matrix(1:3, nc = 3), start = "2010Q2",
+                  names = c("a", "b", "c"))
   ts1 <- as.ts(regts1)
 
   # select two columns
@@ -326,10 +339,11 @@ test_that("start and end/prd_range, univariate", {
   expect_identical(regts(1:10), regts(1:10, start = 1, end = 10))
   expect_identical(regts(1:10), regts(1:10, start = 1))
   expect_identical(regts(1:10), regts(1:10, end = 10))
-  expect_identical(regts(1:10), regts(1:10, period = period_range("1","10")))
+  expect_identical(regts(1:10), regts(1:10, period = period_range("1", "10")))
   expect_identical(regts(rep(1, 10)), regts(1, start = 1, end = 10))
   expect_identical(regts(rep(1:2, 5)), regts(1:2, start = 1, end = 10))
-  expect_identical(regts(rep(1:2, 5)), regts(1:2, period = period_range("1","10")))
+  expect_identical(regts(rep(1:2, 5)),
+                   regts(1:2, period = period_range("1", "10")))
 })
 
 
@@ -337,26 +351,32 @@ test_that("start and end/prd_range, multivariate", {
   data <- matrix(1:10, ncol = 2)
   expect_identical(regts(data), regts(data, start = "1", end = "5"))
   expect_identical(regts(data[1:2, ]), regts(data, start = 1, end = 2))
-  expect_identical(regts(data[1:2, ]), regts(data, period = period_range("1","2")))
+  expect_identical(regts(data[1:2, ]),
+                   regts(data, period = period_range("1", "2")))
   expect_identical(regts(data[1, , drop = FALSE]),
                    regts(data, start = 1, end = 1))
-  expect_identical(regts(data[1, , drop = FALSE], start = 1, end = 2),
-                   regts(rbind(data[1, , drop=FALSE], data[1, , drop = FALSE]),
-                         period = period_range("1","2")))
-
+  expect_identical(
+    regts(data[1, , drop = FALSE], start = 1, end = 2),
+    regts(rbind(data[1, , drop = FALSE], data[1, , drop = FALSE]),
+          period = period_range("1", "2"))
+  )
 })
 
 test_that("start and end/prd_range, multivariate (1 column)", {
   data <- matrix(1:5, ncol = 1)
   expect_identical(regts(data), regts(data, start = "1", end = "5"))
-  expect_identical(regts(data), regts(data, period = period_range("1","5")))
-  expect_identical(regts(data[1:2, , drop = FALSE]), regts(data, start = 1, end = 2))
-  expect_identical(regts(data[1, , drop = FALSE]), regts(data, start = 1, end = 1))
-  expect_identical(regts(data[1, , drop = FALSE]), regts(data, period = period_range("1","1")))
-  expect_identical(regts(data[1, , drop = FALSE], start = 1, end = 2),
-                   regts(rbind(data[1, , drop=FALSE], data[1, , drop = FALSE]),
-                         start = 1, end = 2))
-
+  expect_identical(regts(data), regts(data, period = period_range("1", "5")))
+  expect_identical(regts(data[1:2, , drop = FALSE]),
+                   regts(data, start = 1, end = 2))
+  expect_identical(regts(data[1, , drop = FALSE]),
+                   regts(data, start = 1, end = 1))
+  expect_identical(regts(data[1, , drop = FALSE]),
+                   regts(data, period = period_range("1", "1")))
+  expect_identical(
+    regts(data[1, , drop = FALSE], start = 1, end = 2),
+    regts(rbind(data[1, , drop = FALSE], data[1, , drop = FALSE]),
+          start = 1, end = 2)
+  )
 })
 
 test_that("period selection exotic timeseries", {
@@ -369,19 +389,19 @@ test_that("period selection exotic timeseries", {
                    names = "a")
 
   expect_identical(num_ts["2018Q1", 1], regts(NA_real_, period = "2018Q1"))
-  expect_identical(logi_ts["2013Q4/2014Q1"][, 1], regts(c(NA, FALSE),
-                                                   period = "2013q4/2014q1"))
-  expect_identical(logi_ts["2018Q1/2018Q2"][, 1], regts(NA,
-                                                        period = "2018q1/2018q2"))
+  expect_identical(logi_ts["2013Q4/2014Q1"][, 1],
+                   regts(c(NA, FALSE), period = "2013q4/2014q1"))
+  expect_identical(logi_ts["2018Q1/2018Q2"][, 1],
+                   regts(NA, period = "2018q1/2018q2"))
 
-  expect_identical(txt_ts["2018Q1/2018Q2"][, 1], regts(NA_character_,
-                                                        period = "2018q1/2018q2"))
+  expect_identical(txt_ts["2018Q1/2018Q2"][, 1],
+                   regts(NA_character_, period = "2018q1/2018q2"))
 
-  expect_identical(txt_ts["2014Q2/2014Q3"][, 1], regts(c("y", NA),
-                                                       period = "2014q2/2014q3"))
+  expect_identical(txt_ts["2014Q2/2014Q3"][, 1],
+                   regts(c("y", NA), period = "2014q2/2014q3"))
 
-  expect_identical(txt_ts["2018Q1/2018Q2", 1], regts(NA_character_,
-                                                       period = "2018q1/2018q2"))
+  expect_identical(txt_ts["2018Q1/2018Q2", 1],
+                   regts(NA_character_, period = "2018q1/2018q2"))
 
   # factor timeseries (behaves a little bit strangely)
   factors <- as.factor(c("x", "y"))
@@ -389,7 +409,7 @@ test_that("period selection exotic timeseries", {
   factor_ts <- ts(factors, start = 2014, frequency = 4)
   expect_identical(as.ts(factor_ts), factor_ts)
   expect_identical(as.ts(factor_regts["2018Q1/2018Q2"]),
-                   window(factor_ts, start = c(2018,1), end = c(2018, 2),
+                   window(factor_ts, start = c(2018, 1), end = c(2018, 2),
                           extend = TRUE))
 })
 
@@ -442,8 +462,8 @@ test_that("undefined columns selected", {
 
   regts1 <- regts(matrix(1:10, ncol = 1), start = "2010Q4", names = "a")
 
-  expect_error(regts1[ , NA_character_], "Undefined columns: NA.")
-  expect_error(regts1[ , c("a", "z", "p", "g")], "Undefined columns: z, p, g.")
+  expect_error(regts1[, NA_character_], "Undefined columns: NA.")
+  expect_error(regts1[, c("a", "z", "p", "g")], "Undefined columns: z, p, g.")
 
   tryCatch({
     regts1[, "xxx"]
@@ -490,4 +510,12 @@ test_that("tibble with numeric columns", {
   expect_equal(data_ts2,
                regts(matrix(unlist(data, use.names = FALSE), ncol = 2),
                      names = c("a", "b"), start = start))
+})
+
+test_that("misc errors", {
+  expect_error(
+    get_period_range(1),
+    "Argument is not a timeseries",
+    fixed = TRUE
+  )
 })
