a7e11c6fd4d48a4d24c7c79fc5bdd46e9d967113
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2026-04-13 14:49:05 +0200

    Several improvements.

--- install_deps.R
+++ install_deps.R
@@ -1,6 +1,17 @@
 #!/usr/bin/Rscript
 repo <- "https://cloud.r-project.org"
-if (!require(pak)) {
-  install.packages("pak", repos = repo)
+
+options(repos = c(CRAN = repo)) 
+
+# If the user library does not exist, then create it
+# and add it to the library path.
+user_lib_dir <- Sys.getenv("R_LIBS_USER")
+if (!dir.exists(user_lib_dir)) {
+  dir.create(user_lib_dir, recursive = TRUE)
+  .libPaths(user_lib_dir)
 }
-pak::local_install_deps("pkg", upgrade = FALSE)
+
+if (!require(pak)) {
+  install.packages("pak")
+}
+pak::local_install_dev_deps("pkg", upgrade = FALSE)
